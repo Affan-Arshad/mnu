ef4d371fff8aaffa3cb7023072ef3b8bcaeb919f
Affan-Arshad <35035482+Affan-Arshad@users.noreply.github.com>
2024-06-08 11:29:48 +0500

    important

--- submit.php
+++ submit.php
@@ -7,38 +7,38 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <form action="submitted.php" method="POST">
+    <form action="submitted.php?as=userRegistration" method="POST">
         <div class="input-line">
             <label for="name">name</label>
-            <input type="text" name="userRegistration[name]" id="name" required>
+            <input type="text" name="name" id="name" required>
         </div>
         <div class="input-line">
             <label for="email">email</label>
-            <input type="email" name="userRegistration[email]" id="email" required>
+            <input type="email" name="email" id="email" required>
         </div>
         <div class="input-line">
             <label for="password">password</label>
-            <input type="password" name="userRegistration[password]" id="password" required>
+            <input type="password" name="password" id="password" required>
         </div>
         <div class="input-line">
             <label>Gender</label>
-            <input type="radio" name="userRegistration[gender]" id="male" value="male" required>
+            <input type="radio" name="gender" id="male" value="male" required>
             <label for="male">Male</label>
-            <input type="radio" name="userRegistration[gender]" id="female" value="female" required>
+            <input type="radio" name="gender" id="female" value="female" required>
             <label for="female">Female</label>
         </div>
         <div class="input-line">
             <label>Hobbies</label>
-            <input type="checkbox" name="userRegistration[hobbies[]]" id="gaming" value="gaming">
+            <input type="checkbox" name="hobbies[]" id="gaming" value="gaming">
             <label for="gaming">Gaming</label>
-            <input type="checkbox" name="userRegistration[hobbies[]]" id="sleeping" value="sleeping">
+            <input type="checkbox" name="hobbies[]" id="sleeping" value="sleeping">
             <label for="sleeping">Sleeping</label>
-            <input type="checkbox" name="userRegistration[hobbies[]]" id="coding" value="coding">
+            <input type="checkbox" name="hobbies[]" id="coding" value="coding">
             <label for="coding">Coding</label>
         </div>
         <div class="input-line">
             <label for="age">age</label>
-            <input type="number" name="userRegistration[age]" id="age" required>
+            <input type="number" name="age" id="age" required>
         </div>
 
         <button>Submit</button>
--- submitted.php
+++ submitted.php
@@ -1,11 +1,35 @@
 <?php
 
+if(!isset($_GET['as']) || $_GET['as'] !== 'userRegistration') {
+    header('Location: .');
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$gender = $_POST['gender'];
+$hobbies = $_POST['hobbies'];
+$age = $_POST['age'];
+
 echo "<pre>";
 print_r($_POST);
 echo "</pre>";
 
-if(!isset($_POST['userRegistration'])) {
-    header('Location: .');
+
+$filename = 'data.txt';
+$data = json_encode($_POST) . "\n";
+
+if (is_writable($filename)) {
+
+    if (!$fp = fopen($filename, 'a')) {
+        echo "Cannt open file ($filename)";
+        exit;
+    }
+
+    if (fwrite($fp, $data) === FALSE) {
+        echo "Cannot write to file ($filename)";
+        exit;
+    }
+    
+    echo "Success, wrote ($data) to file ($filename)";
 }
-
-
